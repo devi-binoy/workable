9844a67049f1f0776d7ad17fdb614c47298c4686
madhavanmohan <madhavanmohan2002@gmail.com>
2023-06-14 00:37:11 +0530

    feat: seeker login completed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, createContext,useContext } from "react";
-import { auth,provider } from "../firebase";
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { auth, provider, db } from "../firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, signInWithPopup } from "firebase/auth";
 
 
@@ -11,80 +11,98 @@ export const AuthContextProvider = ({ children }) => {
 
 
     const createUser = (email, password) => {
-        return new Promise ((resolve,reject) => {
-       createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-        resolve(true)
-        // ...
-        })
-        .catch((error) => {
-            reject(error)
+        return new Promise((resolve, reject) => {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    const { uid, email } = user;
+
+                    db.collection("applicants").doc(uid).set({ email })
+                        .then(() => {
+                            resolve(true);
+                        })
+                        .catch((error) => {
+                            reject(error);
+                        });
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
-        });
-    }
+    };
+
 
     const loginUser = (email, password) => {
-        return new Promise ((resolve,reject) => {
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        resolve(true)
-        // ...
-        })
-        .catch((error) => {
-            reject(error)
+        return new Promise((resolve, reject) => {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    const { uid } = user;
+
+                    firestore.collection("applicants").doc(uid).get()
+                        .then((doc) => {
+                            if (doc.exists) {
+                                const userData = doc.data();
+                                console.log(userData.email);
+                            }
+
+                            resolve(true);
+                        })
+                        .catch((error) => {
+                            reject(error);
+                        });
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
-        });
-    }
+    };
+
 
     const loginGoogle = () => {
-            return new Promise ((resolve,reject)=>{
+        return new Promise((resolve, reject) => {
             signInWithPopup(auth, provider)
-            .then((result) => {
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user);
-            resolve(true)
-            // ...
-            }).catch((error) => {
-            // Handle errors here
-           reject(error);
-            });
+                .then((result) => {
+                    // The signed-in user info.
+                    const user = result.user;
+                    console.log(user);
+                    resolve(true)
+                    // ...
+                }).catch((error) => {
+                    // Handle errors here
+                    reject(error);
+                });
         });
 
     }
 
     const logoutUser = () => {
         signOut(auth).then(() => {
-        // Sign-out successful.
+            // Sign-out successful.
         }).catch((error) => {
-        // An error happened
-                
+            // An error happened
+
         });
     }
     useEffect(() => {
-       onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, (user) => {
             if (user) {
-              setUser(user);
+                setUser(user);
             } else {
-              setUser(null);
+                setUser(null);
             }
-          }
+        }
         );
-    
-     }, [user])
+
+    }, [user])
 
     return (
-        <UserContext.Provider value={{ createUser ,loginUser,logoutUser,loginGoogle,user}}>
+        <UserContext.Provider value={{ createUser, loginUser, logoutUser, loginGoogle, user }}>
             {children}
         </UserContext.Provider>
     );
 };
 
-export const UserAuth = ()=>{
+export const UserAuth = () => {
     return useContext(UserContext);
 }
